7bb50bfe99a2b4edd231b0c938164e4af177a872
avnish986 <avnishgupta986@gmail.com>
2020-10-20 21:50:34 +0530

    [Avnish]Added:loaded state data csv

--- src/main/java/com/census/analysis/StateCodeAnalyser.java
+++ src/main/java/com/census/analysis/StateCodeAnalyser.java
@@ -6,29 +6,29 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.stream.StreamSupport;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
-public class StateCensusAnalyser {
-
+public class StateCodeAnalyser {
 	public int loadCSVFile(Path path) throws CensusException {
 		try {
 			Reader reader = Files.newBufferedReader(path);
-			CsvToBean<CSVStateCensus> csvToBean = new CsvToBeanBuilder(reader).withType(CSVStateCensus.class)
+			CsvToBean<StateCode> csvToBean = new CsvToBeanBuilder(reader).withType(StateCode.class)
 					.withIgnoreLeadingWhiteSpace(true).build();
-			
-			Iterator<CSVStateCensus> iterator = csvToBean.iterator();
-			ArrayList<CSVStateCensus> stateCensusList = new ArrayList<>();
-			while (iterator.hasNext()) {
-				stateCensusList.add(iterator.next());
+			Iterator<StateCode> it = csvToBean.iterator();
+			ArrayList<StateCode> stateCensusList = new ArrayList<>();
+			while (it.hasNext()) {
+				stateCensusList.add(it.next());
 			}
-			System.out.println(stateCensusList);
+			for(StateCode x :stateCensusList ) {
+				System.out.println(x);
+			}
 			return stateCensusList.size();
 		} catch (IOException e) {
 			throw new CensusException("File not found", CensusException.ExceptionType.WRONG_CSV);
-		} catch (RuntimeException e) {
+		}
+		catch (RuntimeException e) {
 			throw new CensusException("File internal data not valid", CensusException.ExceptionType.WRONG_HEADER);
 		}
 	}
