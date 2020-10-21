a081db265e2410d754aca4ea74991008312783b2
avnish986 <avnishgupta986@gmail.com>
2020-10-21 17:38:34 +0530

    [Avnish]Refactored: census analyser examining single reponsibilty principle separating csv builder files

--- src/main/java/com/census/analysis/CensusAnalyser.java
+++ src/main/java/com/census/analysis/CensusAnalyser.java
@@ -7,7 +7,6 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.stream.StreamSupport;
-
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
@@ -15,7 +14,8 @@ public class CensusAnalyser<E> {
 	public int loadCSVFile(Path path) throws CensusException {
 		try (Reader reader = Files.newBufferedReader(path);) {
 
-			Iterator<CSVStateCensus> iterator = (Iterator<CSVStateCensus>) this.getFileIterator(reader,CSVStateCensus.class);
+			Iterator<CSVStateCensus> iterator = (Iterator<CSVStateCensus>) new OpenCSVBuilder().getFileIterator(reader,
+					CSVStateCensus.class);
 			return filesize(iterator);
 		} catch (IOException e) {
 			throw new CensusException("File not found", CensusException.ExceptionType.WRONG_CSV);
@@ -25,12 +25,12 @@ public class CensusAnalyser<E> {
 	public int loadStateCSVFile(Path path) throws CensusException {
 		try (Reader reader = Files.newBufferedReader(path)) {
 
-			Iterator<StateCode> it = (Iterator<StateCode>) this.getFileIterator(reader, StateCode.class);
+			Iterator<StateCode> it = (Iterator<StateCode>) new OpenCSVBuilder().getFileIterator(reader,
+					StateCode.class);
 			return filesize(it);
 		} catch (IOException e) {
 			throw new CensusException("File not found", CensusException.ExceptionType.WRONG_CSV);
 		}
-
 	}
 
 	private <E> int filesize(Iterator<E> iterator) {
@@ -43,18 +43,4 @@ public class CensusAnalyser<E> {
 		System.out.println(stateCensusList);
 		return stateCensusList.size();
 	}
-
-	private Iterator<E> getFileIterator(Reader reader, Class csvClass) throws CensusException {
-		try {
-			CsvToBean<E> csvToBean = new CsvToBeanBuilder(reader)
-										.withType(csvClass)
-										.withIgnoreLeadingWhiteSpace(true)
-										.build();
-
-			return csvToBean.iterator();
-		} catch (RuntimeException e) {
-			throw new CensusException("File internal data not valid", CensusException.ExceptionType.WRONG_HEADER);
-		}
-	}
-
 }
